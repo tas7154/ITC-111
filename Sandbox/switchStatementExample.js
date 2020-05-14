b5495bac00aaffceed58e87a309528ab34b2a38d
The Aquarian <tas@tas1.local>
2020-05-14 12:57:29 -0700

    examples

---
+++ Sandbox/switchStatementExample.js
@@ -0,0 +1,21 @@
+// Switch Statement - gives multiple outcomes based on the a value of condition
+
+var seasons = 'Spring';
+
+switch(seasons){
+    case 'Summer':
+        console.log('It\'s Summer!');
+        break;
+    case 'Fall':
+        console.log('It\'s Fall!');
+        break;
+    case 'Winter':
+        console.log('It\'s Winter!');
+        break;
+    case 'Spring':
+        console.log('It\'s Spring!');
+        break;
+    default:
+        console.log('There are only 4 seasons!');
+        break;    
+}
